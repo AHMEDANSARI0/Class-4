114a2aada27495500bc305c725eed4c4bba887d9
AHMEDANSARI0 <161446822+AHMEDANSARI0@users.noreply.github.com>
2024-03-08 21:31:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// yeh code addition k liye he 
+var num1 = 5;
+var num2 = 8;
+var addTotal = num1 + num2;
+console.log(addTotal);
+//yeh code subtraction k liye he
+var subNum1 = 7;
+console.log(addTotal - subNum1);
+//yeh code multiply k liye h
+// yeh code devide k liye he 
+var totalApples = 20;
+var bags = 4;
+var ansawer1 = totalApples / bags;
+console.log(ansawer1);
+// modulus
+var apples = 5;
+var peoples = 2;
+var reminder = (apples % peoples);
+console.log(reminder);
+var baseNumber = 5;
+var powerNumber = 6;
+var power = Math.pow(baseNumber, powerNumber);
+console.log(power);
