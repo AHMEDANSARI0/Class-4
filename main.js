052f7ab5f9e5622054432be1d1b5b112cc4b3bd6
AHMEDANSARI0 <161446822+AHMEDANSARI0@users.noreply.github.com>
2024-03-08 21:51:59 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-// yeh code addition k liye he 
+// yeh code addition k liye he
 var num1 = 5;
-var num2 = 8;
+var num2 = 2;
 var addTotal = num1 + num2;
 console.log(addTotal);
-//yeh code subtraction k liye he
-var subNum1 = 7;
-console.log(addTotal - subNum1);
-//yeh code multiply k liye h
-// yeh code devide k liye he 
-var totalApples = 20;
-var bags = 4;
-var ansawer1 = totalApples / bags;
-console.log(ansawer1);
-// modulus
-var apples = 5;
-var peoples = 2;
-var reminder = (apples % peoples);
-console.log(reminder);
-var baseNumber = 5;
-var powerNumber = 6;
-var power = Math.pow(baseNumber, powerNumber);
-console.log(power);
+// yeh code subtraction k liye he
+var subNum = 3;
+console.log(addTotal - subNum);
---
+++ main1.js
@@ -0,0 +1,20 @@
+// MULTIPILICATION
+var orange = 12;
+var carton = 2;
+var total = orange * carton;
+console.log(total);
+// divide
+var apples = 12;
+var peoples = 3;
+var divide = apples / peoples;
+console.log(divide);
+// modulus
+var mango = 9;
+var boy = 4;
+var reminder = mango % boy;
+console.log(reminder);
+// Power/ Base
+var baseNumber = 25;
+var powerNumber = 8;
+var power = Math.pow(baseNumber, powerNumber);
+console.log(power);
